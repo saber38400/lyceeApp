ce64989dc311f1ecc0ce811a466c76abaa96d64e
Saber B <sbenhaoula@yahoo.fr>
2026-02-01 17:23:29 +0100

    first commit

---
+++ src/main/UserRegistration1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lycee.lycee_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistration1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
